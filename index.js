2c2da43d2a3ecfccad4d31a2d88829042b4a0a54
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2022-11-20 03:32:40 +0100

    update on the server side

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then((
   
     console.log("connected to mongo database")
 })
-app.get("/", (req, res) => {
-    res.send("happy coding")
-})
+  
     .catch(err => console.log(err))
+     app.get("/", (req, res) => {
+       res.send("happy coding")
+    })
    
   
 app.listen(PORT, console.log(`app running on port ${PORT}`))
